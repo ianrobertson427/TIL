2fb9d8cb381edcc1c92d53d7cde419ce5ab1663f
“irobetson” <ianrobertson427@gmail.com>
2020-06-27 13:14:12 -0500

    Creating Public TIL

---
+++ til.rb
@@ -0,0 +1,33 @@
+TIL 6/21
+
+# How to make a simple each loop to iterate through an array that will return the sum of INTS
+
+# EX:
+
+array = [5,2,6,1]
+
+sum = 0
+
+array.each do |number|
+    sum += number
+end
+
+p sum
+
+# Returns 14
+
+# Various Terminal commands including
+-> mkdir to create a directory
+-> cat 
+
+6/22
+
+# Got a console error today saying the new shell is ZSH.  Updated and researched the shell
+
+# learned how to use basic git commands
+
+# git config --global user.name “irobetson”
+
+6/23
+
+# Work today I used packet captures and pinging to determine our int tunnel from our data center was down.  Simply restarted Meraki site-to-site VPN worked
